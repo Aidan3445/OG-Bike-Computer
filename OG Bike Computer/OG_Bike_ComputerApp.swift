c47216c24fd989d4746d89346a29e8154eaef1b2
Aidan Weinberg <aidan3445@gmail.com>
2026-02-27 17:39:16 -0600

    project init

---
+++ OG Bike Computer/OG_Bike_ComputerApp.swift
@@ -0,0 +1,17 @@
+//
+//  OG_Bike_ComputerApp.swift
+//  OG Bike Computer
+//
+//  Created by Aidan Weinberg on 2/27/26.
+//
+
+import SwiftUI
+
+@main
+struct OG_Bike_ComputerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
