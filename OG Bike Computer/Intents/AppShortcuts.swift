5319aab3b0304abf469ceef053e4cb2ef1381130
Aidan Weinberg <aidan3445@gmail.com>
2026-04-12 23:48:21 -0500

    add Siri phrase variations and alternative app name pronunciations

--- OG Bike Computer/Intents/AppShortcuts.swift
+++ OG Bike Computer/Intents/AppShortcuts.swift
@@ -18,7 +18,17 @@ struct AppShortcuts: AppShortcutsProvider {
                 "Start cycling with \(.applicationName)",
                 "Begin a ride in \(.applicationName)",
                 "Start a free ride with \(.applicationName)",
-                "Start recording with \(.applicationName)"
+                "Start recording with \(.applicationName)",
+
+                "Start a \(.applicationName) ride",
+                "Begin a \(.applicationName) ride",
+                "Start a \(.applicationName) free ride",
+                "Start a \(.applicationName) recording",
+                
+                "Start \(.applicationName) ride",
+                "Begin \(.applicationName) ride",
+                "Start \(.applicationName) free ride",
+                "Start \(.applicationName) recording",
             ],
             shortTitle: "Start Ride",
             systemImageName: "bicycle"
@@ -30,7 +40,11 @@ struct AppShortcuts: AppShortcutsProvider {
             phrases: [
                 "Pause my ride in \(.applicationName)",
                 "Pause ride with \(.applicationName)",
-                "Pause cycling in \(.applicationName)"
+                "Pause cycling in \(.applicationName)",
+
+                "Pause my \(.applicationName) ride",
+                "Pause \(.applicationName) ride",
+                "Pause \(.applicationName) cycling",
             ],
             shortTitle: "Pause Ride",
             systemImageName: "pause.fill"
@@ -42,7 +56,11 @@ struct AppShortcuts: AppShortcutsProvider {
             phrases: [
                 "Resume my ride in \(.applicationName)",
                 "Resume ride with \(.applicationName)",
-                "Continue ride in \(.applicationName)"
+                "Continue ride in \(.applicationName)",
+
+                "Resume my \(.applicationName) ride",
+                "Resume \(.applicationName) ride",
+                "Continue \(.applicationName) ride",
             ],
             shortTitle: "Resume Ride",
             systemImageName: "play.fill"
@@ -55,7 +73,13 @@ struct AppShortcuts: AppShortcutsProvider {
                 "End my ride in \(.applicationName)",
                 "Stop ride with \(.applicationName)",
                 "End cycling in \(.applicationName)",
-                "Finish ride with \(.applicationName)"
+                "Finish ride with \(.applicationName)",
+
+                "End my \(.applicationName) ride",
+                "Stop \(.applicationName) ride",
+                "End \(.applicationName) cycling",
+                "Finish \(.applicationName) ride",
+                "End \(.applicationName) ride"
             ],
             shortTitle: "End Ride",
             systemImageName: "stop.fill"
@@ -66,7 +90,10 @@ struct AppShortcuts: AppShortcutsProvider {
             intent: ChangeRouteIntent(),
             phrases: [
                 "Change route in \(.applicationName)",
-                "Switch route in \(.applicationName)"
+                "Switch route in \(.applicationName)",
+
+                "Change \(.applicationName) route",
+                "Switch \(.applicationName) route",
             ],
             shortTitle: "Change Route",
             systemImageName: "arrow.triangle.swap"
@@ -78,7 +105,11 @@ struct AppShortcuts: AppShortcutsProvider {
             phrases: [
                 "Switch to \(\.$profile) profile in \(.applicationName)",
                 "Select \(\.$profile) profile in \(.applicationName)",
-                "Use \(\.$profile) settings in \(.applicationName)"
+                "Use \(\.$profile) settings in \(.applicationName)",
+
+                "Switch \(.applicationName) to \(\.$profile) profile",
+                "Select \(.applicationName) \(\.$profile) profile",
+                "Use \(.applicationName) \(\.$profile) settings",
             ],
             shortTitle: "Select Profile",
             systemImageName: "gearshape.2"
@@ -89,7 +120,10 @@ struct AppShortcuts: AppShortcutsProvider {
             intent: SelectBikeIntent(),
             phrases: [
                 "Switch to \(\.$bike) in \(.applicationName)",
-                "Select \(\.$bike) in \(.applicationName)"
+                "Select \(\.$bike) in \(.applicationName)",
+
+                "Switch \(.applicationName) to \(\.$bike)",
+                "Select \(.applicationName) \(\.$bike)",
             ],
             shortTitle: "Select Bike",
             systemImageName: "bicycle.circle"
